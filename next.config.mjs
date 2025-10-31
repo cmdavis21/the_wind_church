da5f80ac54073a54aebc5c92f6b80e95aeb845e2
Chyna Davis <cdavis2757@gmail.com>
2025-10-31 07:58:47 -0700

    fix unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const nextConfig = {
   compress: true,
-  images: { unoptimized: true },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -15,36 +14,7 @@ const nextConfig = {
     return config;
   },
   images: {
-    remotePatterns: [
-      {
-        // SANITY
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        // SHOPIFY
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        // AWS
-        protocol: 'https',
-        hostname: 'thewindchurch.s3.us-east-2.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        // YOUTUBE
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
