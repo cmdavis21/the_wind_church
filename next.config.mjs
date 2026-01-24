e1f824a3c74622b1d176ebb0f364259c5bf1863a
Chyna Davis <cdavis2757@gmail.com>
2026-01-24 10:52:47 -0800

    converted to standalon output

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import path from 'path';
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const nextConfig = {
+  output: 'standalone',
   compress: true,
   webpack: (config) => {
     config.resolve.alias = {
