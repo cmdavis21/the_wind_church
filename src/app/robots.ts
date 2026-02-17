d75004bd2aeed75bb0ed3052b3dc61b944431e0b
Chyna Davis <cdavis2757@gmail.com>
2026-02-16 20:24:19 -0800

    disallowed robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,14 +2,20 @@ import { WEBSITE_BASE_URL } from '@/data/constants';
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/studio/'],
-      },
-    ],
-    sitemap: `${WEBSITE_BASE_URL}/sitemap.xml`,
-  };
+  const isProd = process.env.NEXT_PUBLIC_APPLICATION_MODE === 'production';
+  return isProd
+    ? {
+        rules: {
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/api/', '/studio/'],
+        },
+        sitemap: `${WEBSITE_BASE_URL}/sitemap.xml`,
+      }
+    : {
+        rules: {
+          userAgent: '*',
+          disallow: '/',
+        },
+      };
 }
