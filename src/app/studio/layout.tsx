5a0d38a668335b8dba3b53365bc9d62a0b627e36
Chyna Davis <cdavis2757@gmail.com>
2026-03-18 14:22:26 -0700

    remove stories and test from forms folder components

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Sanity.io Studio | The Wind Church',
-  description: 'The Sanity.io Dashboard Studio for the Wind Church Website.',
+  description: 'The Sanity.io Dashboard Studio for The Wind Church of Riverside, CA',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
