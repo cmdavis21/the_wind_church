c9410cf26ccb1dc5a2a3644092d8e487eff40850
Chyna Davis <cdavis2757@gmail.com>
2026-03-17 22:07:16 -0700

    remove stories and test from navigation folder components

--- src/components/navigation/navbar/desktop-nav/desktop-nav-item/DesktopNavItem.test.tsx
+++
@@ -1,6 +0,0 @@
-describe('DesktopNavItem', () => {
-  it('should render label', () => {});
-  it('underline grow on hover', () => {});
-  it('underline stays full width on active', () => {});
-  it('if link enabled, have onclick functionality', () => {});
-});
